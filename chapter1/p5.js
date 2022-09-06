579b49680a1772bf4d551b20235a55033ccc4266
crfmc <crf85@cornell.edu>
2022-09-06 19:08:42 -0400

    adds problem 5 solution

--- chapter1/p5.js
+++ chapter1/p5.js
@@ -1,32 +1,58 @@
 /**
  * Question:
- * There are three types of edits that can be performed on strings:
- * insert a character, remove a character, or replace a character.
- * Given two strings, write a function to check if they are one edit
- * (or zero edits) away.
+ * Implement a method to perform basic string compression using the counts of 
+ * repeated characters. For example, the string aabcccccaaa would become 
+ * a2b1c5a3. If the "compressed" string would not become smaller than the 
+ * original string, your method should return the original string. You can 
+ * assume the string has only uppercase and lowercase letters (a - z).
  */
 
 /**
  * Notes:
- */
- const Problem5 = (s) => {
-    return "unimplemented"
+*/
+const Problem5 = (s) => {
+  let char = 0;
+  let counts = new Map();
+
+  for (let i = 0; i <= s.length - 1; i++) {
+    if (counts.has(char) && s[char] === s[i]) {
+      counts.set(char, counts.get(char) + 1);
+    }
+    else {
+      char = i;
+      counts.set(char, 1);
+    }
   }
+
+  let cs = "";
+  counts.forEach((value, key) => {
+    cs = cs + s[key] + value;
+  });
+
+  if (cs.length >= s.length) {
+    return s;
+  }
+  else {
+    return cs;
+  }
+}
+
   
-  
-  // Tests for Problem
-  console.log("Problem 5 Tests running... \n");
-  
-  console.assert( Problem5("pale", "ple") === true, "ex1");
-  console.assert( Problem5("pales", "pale") === true, "ex2");
-  console.assert( Problem5("pale", "bale") === true, "ex3");
-  console.assert( Problem5("pale", "bake") === false, "ex4");
-  
-  
-  
-  module.exports;
-  
-  /**
-   * Notes:
-   */
-  
+// Tests for Problem
+console.log("Problem 5 Tests running... \n");
+
+console.assert( Problem5("aabcccccaaa") === "a2b1c5a3", "ex1");
+// console.assert( Problem5("abcd") === "abcd", "compressed is longer");
+// console.assert( Problem5("") === "", "empty string");
+// console.assert( Problem5("a") === "a", "single letter, compress longer");
+
+
+module.exports;
+
+/**
+ * Notes:
+ *  VERY IMPORTANT NOTE ABOUT MAPS IN JS:
+ * The Map.protorype.forEach() function calls the callback fn(value, key, map).
+ * This means that the value is the first parameter returned, where one
+ * might expect it to be key, then value, as it would be in an object.
+*/

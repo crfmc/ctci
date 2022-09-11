64e51130e24a30fbcb94460b1281edbc9279a1bf
crfmc <crf85@cornell.edu>
2022-09-11 18:31:23 -0400

    adds caveat 1

--- chapter1/p7.js
+++ chapter1/p7.js
@@ -30,12 +30,35 @@ const Problem7 = (m) => {
 console.log("Problem 7 Tests running... \n");
 console.assert(Problem7([[]]) === "", "empty matrix");
 console.assert(Problem7([[1]]) === "1", "single value matrix");
-console.assert(Problem7([[1,2], [3,4]]) === "3,1,4,2", "small matrix");
+console.assert(Problem7([[1, 2], [3, 4]]) === "3,1,4,2", "small matrix");
+console.assert(Problem7([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) === "7,4,1,8,5,2,9,6,3",
+  "larger matrix");
 
 
 
 module.exports;
 
 /**
- * Notes:
+ * Notes: 
+ * Let's study this line:
+ * 
+ * let m0 = Array.from({length:len}, () => {
+ *  return new Array(len).fill(0);
+ * });
+ * 
+ * One important thing to note about using Array.prototyope.fill(), 
+ * is that with numbers and strings it works fine. But note, that 
+ * with objects, such as when creating two-dimensional arrays,
+ * it will assign the same object to all positions in the array.
+ * (See CAVEAT 1 below). In order to avoid this issue, we choose
+ * to use a lambda function (anonymous). Here, we used an object 
+ * to ACT as an array, by giving it a property of length, which we 
+ * set to the variable len. By doing this, we are givent a new array
+ * for each value in a "fake" array of length len.
  */
+
+// CAVEAT 1
+// let a = new Array(2).fill(new Array(2).fill(0));
+// console.log(a[0] === a[1]);
+// let b = new Array(2).fill({1: "hi"});
+// console.log(b[0] === b[1]);
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const ch1 = require('./chapter1/ch1.js');
 
-const scratch = require('./scratchpad/index.js');
+// const scratch = require('./scratchpad/index.js');

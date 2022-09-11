279f9d21deb4547ea06fce0644cf295df01da0a8
crfmc <crf85@cornell.edu>
2022-09-11 17:06:11 -0400

    adds more on static keyword

--- scratchpad/bitwise.js
+++ scratchpad/bitwise.js
@@ -25,11 +25,26 @@ module.exports;
 /**
  * On the [static] keyword:
  * 
+ * Per MDN:
+ * "
+ * defines a static method or property for a class, or a class
+ * static initialization block. Neither static methods nor static
+ * properties can be called on instances of the class. Instead, 
+ * they are called on the class instead.
+ * 
+ * [Static functions] are often utility functions, such as functions
+ * to create or clone objects, whereas static properties are useful
+ * are useful for caches, fixed-configuration, or any other data you
+ * don't need to be replicated across instances.
+ * "
+ * 
+ * - Properties
  * In OOP, it is useful to sometimes have fields and functions that 
  * apply to a class, not only to instances of it. For example,
  * if you have a class Fruit, and there is a description of it,
  * it does not need to be initialized as a field of every instances,
  * just once in the class initialization.
+ * 
  * 
  * 
  */

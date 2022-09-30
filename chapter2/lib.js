1ea29186575c4c54b28ca6751ed6deb0b157b3c7
crfmc <crf85@cornell.edu>
2022-09-30 17:09:36 -0400

    implements library for linked list problems

---
+++ chapter2/lib.js
@@ -0,0 +1,85 @@
+/**
+ * Chapter two is devoted to Linked Lists, so I am adding some
+ * classes that will make them easier to work with.
+ */
+
+// The Node representing a Node reresented linked list
+class Node {
+  constructor(val, next=null) {
+    this.val = val;
+    this.next = next;
+  }
+  print() {
+    let s = "" + this.val;
+    let n = this.next;
+    while (n !== null) {
+      s += " -> " + n.val;
+      n = n.next
+    }
+    return s + " -> null"
+  }
+  // Uses the runner technique to intercalate values
+  // Note: assumes even number of nodes
+  weave() {
+    let head = this;
+    let p1 = this.next; // fast pointer
+    let p2 = head; // slow pointer
+
+    while (p1.next !== null) {
+      p1 = p1.next.next;
+      p2 = p2.next;
+    }
+
+    // console.log("second loop");
+    p1 = head;
+    p2 = p2.next;
+
+    while (p2.next !== null) {
+      let aux = p2;
+      p2 = p2.next;
+      aux.next = p1.next;
+      p1.next = aux;
+      p1 = p1.next.next;
+    }
+
+    p1.next = p2;
+  }
+}
+
+// A node representing a node in a linked list object
+class LLNode {
+  constructor(val, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+// A class representing a linkedlist data structure
+class LL {
+  constructor(n = null) {
+    if (n !== null) this.size = 1;
+    this.head = n;
+    this.tail = n;
+  }
+  insert(val) {
+    const newNode = new LLNode(val);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.size = this.size + 1;
+  }
+  print() {
+    let s = "" + this.head.val;
+    let n = this.head.next;
+    while (n !== null) {
+      s += " -> " + n.val;
+      n = n.next
+    }
+    return s + " -> null"
+  }
+}
+
+module.exports = {
+  Node,
+  LLNode,
+  LL
+}
---
+++ chapter2/p1.js
@@ -0,0 +1,16 @@
+const { Node, LLNode, LL } = require('./lib');
+
+/**
+ * Notes:
+ */
+const Problem1 = (s) => {
+}
+
+
+// console.log('Problem 1 tests running... \n');
+// console.assert();
+// console.assert();
+// console.assert();
+
+
+module.exports;

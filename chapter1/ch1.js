2c9bfd9e171011ff2c1babc76022dbfb19b98937
crfmc <crf85@cornell.edu>
2022-09-29 23:14:04 -0400

    includes text given solution to problem 8

--- chapter1/ch1.js
+++ chapter1/ch1.js
@@ -1,6 +1,3 @@
-// require('./p1.js');
-// require('./p2.js');
-// require('./p3.js');
-// require('./p4.js');
-// require('./p6.js');
-require('./p7.js');
+((filename) => {
+  require('./' + filename + '.js');
+})('p9');

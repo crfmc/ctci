21988783802031e575cf775c3c7b5a5461d1b116
Cesar Ferreyra-Mansilla <cferreyramansilla@gmail.com>
2022-10-03 16:02:11 -0400

    Answer first question for chapter 2

--- chapter2/p1.js
+++ chapter2/p1.js
@@ -3,12 +3,34 @@ const { Node, LLNode, LL } = require('./lib');
 /**
  * Notes:
  */
-const Problem1 = (s) => {
+const Problem1 = (n) => {
+    let head = n;
+    let prev = null;
+    let seen = new Set();
+
+    while (n !== null) {
+        
+        if (seen.has(n.val)) { // if already seen, remove it
+            if (prev !== null) {
+                prev.next = n.next
+                prev = prev
+            }
+        }
+        else {
+            // console.log("added")
+            seen.add(n.val)
+            prev = n;
+        }
+        n = n.next;
+    }
+
+    return head
 }
-
-
-// console.log('Problem 1 tests running... \n');
-// console.assert();
+console.log('Problem 1 tests running... \n');
+console.assert(Problem1(new Node(0)).print() === "0 -> null", "empty single node");
+console.assert(Problem1(new Node(0, new Node(0))).print() === "0 -> null", "duplicate nodes");
+console.assert((Problem1(Node.fromArray([2,2,2,2,2]))).print() === "2 -> null", "single char duplicates");
+console.assert((Problem1(Node.fromArray([0,2,0,2,2]))).print() === "0 -> 2 -> null", "interspersed values");
 // console.assert();
 // console.assert();
 

a028d6ac03d3abc15f2d50a00783eab5900eab35
Cesar Ferreyra-Mansilla <cferreyramansilla@gmail.com>
2022-10-05 19:49:45 -0400

    add solution for problem 3

--- chapter2/p3.js
+++ chapter2/p3.js
@@ -1,33 +1,37 @@
+const { Node } = require('./lib');
 /**
  * Question:
- *  Write a method to replace all spaces in a string with '%20': 
- *  You may assume that the string has sufficient space at the end 
- *  to hold the additional characters, and that you are given the "true" 
- *  length of the string.
+ * Delete Middle Node: Implement an algorithm to delete a node in the middle 
+ * (i.e., any node but the first and last node, not necessarily the exact middle)
+ * of a singly linked list, given only access to that node.
  */
 
+
 /**
- * Notes:
- *  I can split the string and join them
+ * Notes: this one came pretty naturally, that's kinda good. Sort of a simple one.
  * 
- * Clarifying questions:
- *  - can I assume there are no trailing spaces? AKA f(' ') => '%20'?
- *  - can I assume there are no leading spaces?
  */
-const Problem3 = (s, len) => {
-  s = s.substring(0, len);
-  return s.split(' ').join('%20')
+const Problem3 = (n) => {
+  if (n.next === null) {
+    throw new Error('this node is the last in the list, there is not middle node.')
+  }
+  n.val = n.next.val;
+  n.next = n.next.next;
+  return
+}
+
+const Problem3_tester = (a, idx) => {
+  let ll = Node.fromArray(a);
+  Problem3(ll.nodeAt(idx));
+  return ll.print();
 }
 
 
 // Tests for Problem 3
 console.log("Problem 3 Tests running... \n");
-
-console.assert(Problem3('', 0) === '', 'empty string');
-console.assert(Problem3('hello world', 11) === 'hello%20world', 'same word');
-console.assert(Problem3(' ', 1) === '%20', 'single space');
-console.assert(Problem3('  ', 1) === '%20', 'multiple space');
-console.assert(Problem3('Mr John Smith   ', 13) === 'Mr%20John%20Smith', 'ctci');
+console.assert(Problem3_tester([1, 2, 3, 4], 1) === '1 -> 3 -> 4 -> null', 'Deleting second value');
+console.assert(Problem3_tester([1, 2, 3, 4], 2) === '1 -> 2 -> 4 -> null', 'Deleting third value');
+console.assert(Problem3_tester([1, 2, 3], 1) === '1 -> 3 -> null', 'Deleting middle value');
 
 
 

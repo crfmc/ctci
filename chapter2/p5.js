13c8e29977b747036f48e2ab2649a64c7e719636
Cesar Ferreyra-Mansilla <cferreyramansilla@gmail.com>
2022-10-17 19:49:55 -0400

    add problem 5 initial solution

--- chapter2/p5.js
+++ chapter2/p5.js
@@ -7,24 +7,49 @@ const { Node } = require('./lib');
 
 /**
  * Notes: 
+ * Assumes both linked lists are not null, AND that they are
+ * of the same length
  * 
  */
-const Problem5 = (n) => {
-  return n
+
+const getNum = (n) => {
+  let s = "";
+  while ( n !== null ) {
+    s = n.val + s;
+    n = n.next;
+  }
+  return parseInt(s);
+}
+const Problem5 = (n1, n2) => {
+
+  let sum = getNum(n1) + getNum(n2);
+  let sumString = "" + sum;
+  let len = sumString.length;
+
+  let first = new Node(sumString[len - 1]);
+  let i = len - 2;
+  let prev = first;
+
+  while ( i >= 0 ) {
+    let char = sumString[i];
+    let curr = new Node(parseInt(char));
+    prev.next = curr;
+    prev = curr;
+    i--;
+  }
+
+  return first
 }
 
-const Problem5_tester = (a, idx) => {
-  let ll = Node.fromArray(a);
-  Problem5(ll.nodeAt(idx));
-  return ll.print();
-}
-
+console.log(parseInt("") )
 
 // Tests for Problem 3
 console.log("Problem 5 Tests running... \n");
-console.assert(Problem5_tester([1, 2, 3, 4], 1) === '1 -> 3 -> 4 -> null', 'Deleting second value');
-console.assert(Problem5_tester([1, 2, 3, 4], 2) === '1 -> 2 -> 4 -> null', 'Deleting third value');
-console.assert(Problem5_tester([1, 2, 3], 1) === '1 ->   3 -> null', 'Deleting middle value');
+console.assert(Problem5(Node.fromArray([0]), Node.fromArray([1])).print() === '1 -> null', 'single value arrays');
+console.assert(Problem5(Node.fromArray([0,0,0]), Node.fromArray([1,1,1])).print() === '1 -> 1 -> 1 -> null', 'multiple values');
+console.assert(Problem5(Node.fromArray([7,1,6]), Node.fromArray([5,9,2])).print() === '2 -> 1 -> 9 -> null', 'book provided test');
+// console.assert(Problem5_tester([1, 2, 3, 4], 2) === '1 -> 2 -> 4 -> null', 'Deleting third value');
+// console.assert(Problem5_tester([1, 2, 3], 1) === '1 ->   3 -> null', 'Deleting middle value');
 
 
 

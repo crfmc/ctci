f4e667c8186f33cfec1b8c9d5b8fd58ada10e619
Cesar Ferreyra-Mansilla <cferreyramansilla@gmail.com>
2022-10-26 19:39:42 -0400

    Implement a Stack data structure

--- chapter2/lib.js
+++ chapter2/lib.js
@@ -101,8 +101,45 @@ class LL {
   }
 }
 
+/**
+ * It seems helpful to have a class for a Stack, I want this
+ * object to have the ability to:
+ * remove the top element in O(1) time,
+ * add elements in O(1) time,
+ * carry the length.
+ */
+
+class Stack {
+  constructor(arr = []) {
+    this.items = arr;
+    this.size = this.items.length;
+  }
+  push(val) {
+    this.items.push(val);
+    this.size++;
+  }
+  peek() {
+    return this.items[this.size - 1];
+  }
+  pop() {
+    if (this.size < 1) {
+      throw RangeError('This stack does not have any values to pop.');
+    }
+    this.size--;
+    return this.items.pop();
+  }
+  clear() {
+    this.items.splice(0, this.size);
+    this.size = 0;
+  }
+  static fromArray(a) {
+    return new Stack(a);
+  }
+}
+
 module.exports = {
   Node,
   LLNode,
-  LL
+  LL,
+  Stack
 }

78a9c10e97e669f3625969eb2011b6877917c91f
crfmc <crf85@cornell.edu>
2022-11-17 21:17:32 -0500

    implement solution for problem 2

--- chapter3/p2.js
+++ chapter3/p2.js
@@ -9,8 +9,78 @@ const { Node, LLNode, LL } = require('../lib');
 
 /**
  * Notes:
+ * Initially I thought about reserving some spot on the
+ * list to holding the minimum value, but the issue here 
+ * was that this would need to be updted constantly and
+ * in O(n) when the minimum is popped.
+ * 
+ * I needed to keep the minimum value close to the
+ * second to minimum value, and it occured to me to
+ * dedicate a field to it in each node.
+ * 
+ * Below is an O(1) additional space solution:
  *  
  */
+
+/**
+ * Class for a node within a linked list abstraction
+ * of a Stack.
+ * 
+ */
+class MinStackNode {
+  constructor(data, next=null, min=this) {
+    this.data = data;
+    this.next = next;
+    this.min = min;
+  }
+}
+
+
+class MinStackLL {
+  constructor(val) {
+    if (typeof (val) === "undefined") {
+      this.top = null;
+    }
+    else {
+      this.top = new MinStackNode(val);
+    }
+  }
+  push(val) {
+    let update_min = val < this.top.min.data;
+    let new_top = new MinStackNode(val, this.top, this.top.min);
+    if (update_min) {
+      new_top.min = new_top;
+    }
+    this.top = new_top;
+  }
+  pop() {
+    if (this.top === null) {
+      throw RangeError('There are no elements to remove');
+    }
+    else {
+      this.top = this.top.next;
+    }
+  }
+  min() {
+    if (this.top === null) {
+      throw RangeError('There are no elements to remove');
+    }
+    else {
+      return this.top.min;
+    }
+  }
+}
+
+let s = new MinStackLL(2);
+s.push(0);
+s.push(1);
+s.push(3);
+s.push(-1);
+s.push(-2);
+s.pop();
+s.pop();
+console.log(s.top.min.data);
+
 const Problem2 = () => {
   return "unimplemented"
 }

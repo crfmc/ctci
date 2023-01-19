6befed7883b7612a59d0c0951db6c54f9babcc56
Cesar Ferreyra-Mansilla <cferreyramansilla@gmail.com>
2023-01-18 22:26:42 -0500

    chore: add template for p2

---
+++ chapter4/p2.js
@@ -0,0 +1,34 @@
+const { LLNode, Graph, GraphNode, GraphAL } = require('../lib/lib');
+const { test_graph_0 } = require('../lib/testing/graphs');
+
+/**
+ * Question: Given a sorted (increasing order) array with unique integer
+ * elements, write an algorithm to create a binary seach tree with 
+ * minimal height.
+ */
+
+/**
+ * Notes: 
+ * Here the question is asking me to create a BST from an array. This should
+ * not be complicated, since I already have a class for it which almost 
+ * supports bubbling up. It will essentially need to
+ */
+const Problem1 = (graph, idx1, idx2) => {
+  return graph.DFS(idx1, idx2);
+}
+
+console.log('Problem 1 tests running... \n');
+console.assert(Problem1(test_graph_0, 0, 0) === true, "search for itself");
+console.assert(Problem1(test_graph_0, 0, 1) === true, "search for child");
+console.assert(Problem1(test_graph_0, 0, 2) === true, "search for other child");
+console.assert(Problem1(test_graph_0, 0, 3) === true, "search for child of child");
+console.assert(Problem1(test_graph_0, 0, 4) === true, "search for child of child of child");
+console.assert(Problem1(test_graph_0, 0, 5) === false, "search for unreachable node");
+console.assert(Problem1(test_graph_0, 1, 2) === false, "search for other child");
+
+
+module.exports;
+
+/**
+ * Notes:
+ */

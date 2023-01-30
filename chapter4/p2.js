30da4e095e104b625eac67c86f01f3e2f397debc
crfmc <crf85@cornell.edu>
2023-01-30 08:55:55 -0500

    chore: update current problem

--- chapter4/p2.js
+++ chapter4/p2.js
@@ -1,5 +1,4 @@
-const { LLNode, Graph, GraphNode, GraphAL } = require('../lib/lib');
-const { test_graph_0 } = require('../lib/testing/graphs');
+const { BSTreeNode } = require('../lib/lib');
 
 /**
  * Question: Given a sorted (increasing order) array with unique integer
@@ -9,23 +8,30 @@ const { test_graph_0 } = require('../lib/testing/graphs');
 
 /**
  * Notes: 
- * Here the question is asking me to create a BST from an array. This should
- * not be complicated, since I already have a class for it which almost 
- * supports bubbling up. It will essentially need to
+ * Here the question is asking me to create a BST from an array.
  */
-const Problem1 = (graph, idx1, idx2) => {
-  return graph.DFS(idx1, idx2);
+const Problem2 = (a) => {
+  return BSTreeNode.fromArray(a);
 }
 
 console.log('Problem 1 tests running... \n');
-console.assert(Problem1(test_graph_0, 0, 0) === true, "search for itself");
-console.assert(Problem1(test_graph_0, 0, 1) === true, "search for child");
-console.assert(Problem1(test_graph_0, 0, 2) === true, "search for other child");
-console.assert(Problem1(test_graph_0, 0, 3) === true, "search for child of child");
-console.assert(Problem1(test_graph_0, 0, 4) === true, "search for child of child of child");
-console.assert(Problem1(test_graph_0, 0, 5) === false, "search for unreachable node");
-console.assert(Problem1(test_graph_0, 1, 2) === false, "search for other child");
 
+// console.assert(Problem1(test_graph_0, 0, 0) === true, "search for itself");
+// console.assert(Problem1(test_graph_0, 0, 1) === true, "search for child");
+// console.assert(Problem1(test_graph_0, 0, 2) === true, "search for other child");
+// console.assert(Problem1(test_graph_0, 0, 3) === true, "search for child of child");
+// console.assert(Problem1(test_graph_0, 0, 4) === true, "search for child of child of child");
+// console.assert(Problem1(test_graph_0, 0, 5) === false, "search for unreachable node");
+// console.assert(Problem1(test_graph_0, 1, 2) === false, "search for other child");
+
+
+
+let t1 = Problem2([0, 1, 2, 3, 4, 5]);
+let inOrderTrav = "012345";
+let ans = "";
+t1.traversePostOrder(n => ans += n.val);
+console.log(ans)
+// console.assert(inOrderTrav === BSTreeNode.traverseInOrder(t1, (n => ans += n.val)));
 
 module.exports;
 

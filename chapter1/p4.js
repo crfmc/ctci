474ea0352de294ff45ca50e92792d2e584217547
crfmc <crf85@cornell.edu>
2023-03-03 13:09:27 -0500

    chore: ch1p4 redo

--- chapter1/p4.js
+++ chapter1/p4.js
@@ -7,6 +7,53 @@
  *  words.
  */
 
+
+
+/**
+ * First, I would ask if the charaters are case sensitive. If not,
+ * we can begin by making them all lowercase.
+ * 
+ * To be a palindrome this string has to have even number counts for
+ * each of its chars except for 1, since it needs to be the same 
+ * backwards and forwards.
+ * 
+ * I can store the counts in a map, then make sure that at most one
+ * value has an odd number of counts.
+ * 
+ * Remember to filter the string for the needs of the problem.
+ * 
+ */
+
+const Problem4_r0 = (string) => {
+  if (string.length < 2) return true
+  let s = string.toLowerCase().replace(" ", "");
+
+
+  let counts = new Map();
+
+  for (let c of s) {
+    if (counts.has(c)) {
+      counts.set(c, counts.get(c) + 1);
+    }
+    else {
+      counts.set(c, 1);
+    }
+  }
+
+  let odds = 0;
+
+  counts.forEach((v, k) => {
+    if (v % 2 !== 0) { // Odd number counts for char k
+      return false
+    }
+  });
+
+
+  return true
+}
+
+
+
 /**
  * Notes:
  *  This function must return whether a permutation of [s] is a palindrome.
@@ -48,9 +95,9 @@ const Problem4 = (string) => {
 // Tests for Problem
 console.log("Problem 4 Tests running... \n");
 
-console.assert(Problem4("")===true, "empty string");
-console.assert(Problem4("s")===true, "single character");
-console.assert(Problem4("Tact coa")===true, "ctci example");
+console.assert(Problem4_r0("")===true, "empty string");
+console.assert(Problem4_r0("s")===true, "single character");
+console.assert(Problem4_r0("Tact coa")===true, "ctci example");
 
 
 /** Keeps a counter of odd numbered character counts */
@@ -85,11 +132,11 @@ const Problem4_1 = (string) => {
 
 
 // Tests for Problem
-console.log("Problem 4 Tests running... \n");
+console.log("Problem 4_1 Tests running... \n");
 
-// console.assert(Problem4_1("")===true, "empty string");
-// console.assert(Problem4_1("s")===true, "single character");
-// console.assert(Problem4_1("Tact coa")===true, "ctci example");
+console.assert(Problem4_1("")===true, "empty string");
+console.assert(Problem4_1("s")===true, "single character");
+console.assert(Problem4_1("Tact coa")===true, "ctci example");
 console.assert(Problem4_1("Tik c oalr")===false, "can't balance");
 
 module.exports;
